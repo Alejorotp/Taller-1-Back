a8c97bfc50e0d6cc682344ef1b6440987f48380a
SEBASTIAN BARRETO MARTINEZ <sebastianbarreto@uninorte.edu.co>
2026-02-27 10:51:18 -0500

    get_n_monsters

--- app.js
+++ app.js
@@ -7,5 +7,30 @@ async function get_monsters_list() {
     return data;
 }
 
-const monster = await get_monsters_list();
-console.log(monster);
+let monster = await get_monsters_list();
+let list = []
+
+for (let m of monster.results){
+    list.push(m.index)
+    monster = list
+}
+
+
+const get_n_monsters = async (n) => {
+    const urls = monster.slice(0,n).map(index => `https://www.dnd5eapi.co/api/monsters/${index}`)
+    const promises = urls.map(url => fetch(url));
+    const responses = await Promise.all(promises);
+    
+    const data = await Promise.all(responses.map(r => r.json()))
+    console.log(data)
+    return data;
+}
+
+const monsters = get_n_monsters(4)
+
+
+console.log(monsters)
+
+
+
+

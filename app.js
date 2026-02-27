36fac608274c852c5294ed28e622bbab754c1b85
Alejandro Toro <alejandropinedo@uninorte.edu.co>
2026-02-27 10:20:41 -0500

    Get monsters: Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-.
+async function get_monsters_n() {
+
+    const response = await fetch("https://www.dnd5eapi.co/api/monsters");
+
+    const data = await response.json();
+
+    return data;
+}
+
+const monster = await get_monsters_n();
+console.log(monster);
